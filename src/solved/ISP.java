ec0cc5f543cbd74164f15bb0b141d508ce6a7b16
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-22 20:54:29 -0600

    Finished ISP Solve

--- src/solved/ISP.java
+++ src/solved/ISP.java
@@ -1,6 +1,10 @@
 package solved;
 
+//This violation is solved because each notification interface is no forced implementation
+//'ThisMessagingService' can now implement 1 or 2 notification services instead of all 3 if needed.
+//The interfaces have also been separated and are only responsible for specific notifications each
 public class ISP {
+    //defining interfaces for email, text, and push notifications
     interface EmailNotification {
         void sendEmail(String recipient, String message);
     }
@@ -13,13 +17,16 @@ public class ISP {
         void sendPushNotification(String deviceId, String message);
     }
 
+    //Define ThisMessagingService that implements notifications
     class ThisMessagingService implements EmailNotification, TextNotification, PushNotification{
         @Override
         public void sendEmail(String recipient, String message){
         }
         @Override
         public void sendText(String phoneNumber, String message){
-            
+        }
+        @Override
+        public void sendPushNotification(String deviceId, String message){
         }
     }
 }
