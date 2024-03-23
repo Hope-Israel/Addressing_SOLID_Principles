ea9bd72580dab73535669099dd4f1119b2a53a82
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-22 20:42:00 -0600

    Adding method implementation

--- src/solved/ISP.java
+++ src/solved/ISP.java
@@ -8,4 +8,18 @@ public class ISP {
     interface TextNotification {
         void sendText(String phoneNumber, String message);
     }
+
+    interface PushNotification {
+        void sendPushNotification(String deviceId, String message);
+    }
+
+    class ThisMessagingService implements EmailNotification, TextNotification, PushNotification{
+        @Override
+        public void sendEmail(String recipient, String message){
+        }
+        @Override
+        public void sendText(String phoneNumber, String message){
+            
+        }
+    }
 }
