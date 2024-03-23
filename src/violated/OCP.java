13c72e9f696f9fae4f0b8a42219010c0c33f148d
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-22 21:20:42 -0600

    Finished OCP solve

--- src/violated/OCP.java
+++ src/violated/OCP.java
@@ -1,11 +1,22 @@
 package violated;
 
+//This code violates OCP because it requires modifying the
+//position method in order to add a new employee posiiton
+//such as manager
 public class OCP {
     public class Employee {
         private String type;
 
         public Employee(String type) {
             this.type = type;
+        }
+
+        public void position() {
+            if (type.equals("Cashier")){
+                System.out.println("Cashier is working");
+            } else if (type.equals("Bartender")){
+                System.out.println("Bartender is working");
+            }
         }
     }
 }
