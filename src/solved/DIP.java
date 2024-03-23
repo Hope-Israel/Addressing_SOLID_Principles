485a076d2bab7566e06bb15898559bede4d90de3
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-22 21:14:17 -0600

    finished DIP Solve

--- src/solved/DIP.java
+++ src/solved/DIP.java
@@ -1,4 +1,40 @@
 package solved;
 
+//This aligns with DIP because the person class now depends on the car interface
+//instead of the car class.
+
 public class DIP {
+
+    //car interface
+    interface Car {
+        void start();
+        void stop();
+    }
+
+    //Person class
+    class Person {
+        private Car car;
+
+        public Person(Car car){
+            this.car = car;
+        }
+
+        //Method to drive car
+        public void drive() {
+            car.start();
+            car.stop();
+        }
+
+
+        //implementing car interface with Vehicle class
+        class Vehicle implements Car {
+            public void start() {
+
+            }
+
+            public void stop(){
+
+            }
+        }
+    }
 }
--- src/violated/DIP.java
+++ src/violated/DIP.java
@@ -1,21 +1,33 @@
 package violated;
 
+//This is a violation of DIP because the Person class depends on the
+//Car class to function. The Person high level module depends on the low-level car modules
 public class DIP {
-    class Driver {
+
+    //person class
+    class Person {
         private Car car;
 
-        public Driver() {
+        public Person() {
             this.car = new Car();
         }
-        public voide drive(){
+
+        // drive method
+        public void drive(){
             car.start();
             car.stop();
         }
     }
 
+    //car class with start and stop methods
     class Car{
         public void start() {
-            
+
         }
+
+        public void stop(){
+
+        }
+
     }
 }
