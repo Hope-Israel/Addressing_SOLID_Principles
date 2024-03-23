4be0c6939bc49503576393f4e5641679ee3eaebc
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-22 20:36:08 -0600

    ISP Violation Complete

--- src/violated/ISP.java
+++ src/violated/ISP.java
@@ -1,9 +1,37 @@
 package violated;
 
+//This is a representation of the Interface Segregation Principle because when a client uses this service, it
+//requires the usage of all three methods even when it may not be necessary or required.
 public class ISP {
-    interface NotificationService {
+
+//Interface called Messaging Service
+    interface MessagingService {
+
+        //methods to send email, text, and push notifications
         void sendEmail(String recipient, String message);
         void sendText(String phoneNumber, String message);
         void sendPushNotification(String deviceId, String message);
     }
+
+
+    //class named thisMessagingService that implements Messaging service
+    class ThisMessagingService implements MessagingService {
+
+        //implementation of methods
+        @Override
+        public void sendEmail(String recipient, String message){
+
+        }
+        @Override
+        public void sendText(String phoneNumber, String message){
+
+        }
+
+        @Override
+        public void sendPushNotification(String deviceId, String message){
+
+        }
+    }
+
+
 }
