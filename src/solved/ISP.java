b4a755ec8637f5cacdc774b409468601c7ad293e
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-22 20:38:06 -0600

    Beginning ISP solved

--- src/solved/ISP.java
+++ src/solved/ISP.java
@@ -1,4 +1,11 @@
 package solved;
 
 public class ISP {
+    interface EmailNotification {
+        void sendEmail(String recipient, String message);
+    }
+
+    interface TextNotification {
+        void sendText(String phoneNumber, String message);
+    }
 }
