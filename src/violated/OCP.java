d76f573f43e1b1363ea482ab6a6a56791baec3b9
Hope-Israel <130772383+Hope-Israel@users.noreply.github.com>
2024-03-22 21:16:40 -0600

    Starting OCP Violation

---
+++ src/violated/OCP.java
@@ -0,0 +1,11 @@
+package violated;
+
+public class OCP {
+    public class Employee {
+        private String type;
+
+        public Employee(String type) {
+            this.type = type;
+        }
+    }
+}
